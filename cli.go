cc28b9e382e8078c180104bae799df4d8518d044
hachirog <hachiro@grape80.com>
2022-12-18 14:07:59 +0900

    Update files. close #3

--- cli.go
+++ cli.go
@@ -1,6 +1,10 @@
 package tmpl2html
 
-import "io"
+import (
+	_ "embed"
+	"fmt"
+	"io"
+)
 
 type CLI struct {
 	Stdout io.Writer
@@ -8,6 +12,41 @@ type CLI struct {
 	Args   []string
 }
 
-func (cli *CLI) Run() (exitcode int) {
-	return exitcode
+const (
+	errorExitCode = 1
+)
+
+func (cli *CLI) Run() (exitCode int) {
+	if err := cli.runContext(); err != nil {
+		fmt.Fprintf(cli.Stderr, "%v\n", err)
+		return errorExitCode
+	}
+	return exitCode
+}
+
+func (cli *CLI) runContext() (err error) {
+	if exit, err := cli.parseArgs(); err != nil {
+		return err
+	} else if exit == true {
+		return nil
+	}
+
+	return err
+}
+
+const (
+	CMD_HELP = "help"
+)
+
+//go:embed embed/usage.txt
+var usage []byte
+
+func (cli *CLI) parseArgs() (exit bool, err error) {
+	switch cli.Args[1] {
+	case CMD_HELP:
+		_, err = fmt.Fprintf(cli.Stdout, "%s\n", usage)
+		return true, err
+	}
+
+	return exit, err
 }
---
+++ cli_test.go
@@ -0,0 +1,55 @@
+package tmpl2html_test
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/grape80/tmpl2html"
+)
+
+type OSArgsTest struct {
+	programName string
+	command     string
+	options     []string
+	tmpl        string
+}
+
+const (
+	programName = "tmpl2html"
+)
+
+func TestCLI_Run(t *testing.T) {
+	tests := map[string]struct {
+		OSArgsTest
+		exitCodeExpected int
+	}{
+		"help":              {OSArgsTest{programName, tmpl2html.CMD_HELP, []string{}, ""}, 0},
+		"help_useless_args": {OSArgsTest{programName, tmpl2html.CMD_HELP, []string{"--option=useless"}, "useless.tmpl"}, 0},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(tt *testing.T) {
+			args := []string{
+				test.programName,
+				test.command,
+				strings.Join(test.options, " "),
+				test.tmpl,
+			}
+
+			var stdout = new(bytes.Buffer)
+			var stderr = new(bytes.Buffer)
+			cli := &tmpl2html.CLI{
+				Args:   args,
+				Stdout: stdout,
+				Stderr: stderr,
+			}
+
+			exitCode := cli.Run()
+			if exitCode != test.exitCodeExpected {
+				t.Errorf("parseArgs() = %v; want %v", exitCode, test.exitCodeExpected)
+			}
+		})
+	}
+}
